2144670c1a0a9849756e2b5967c259d548c11006
Nikita <nikita.pro.pro@yandex.ru>
2023-03-31 02:20:02 +0300

    commit

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface FilmStorage {
     List<Film> getFilms();
+
     Film create(Film film);
+
     Film update(Film film);
+
     Film getFilmById(Long filmId);
+
     Film delete(Long filmId);
 }
