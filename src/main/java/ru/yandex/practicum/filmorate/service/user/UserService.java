9850d5193fbc5926c43f5bd0a45d0fb102ce4af5
Nikita <nikita.pro.pro@yandex.ru>
2023-03-31 02:16:14 +0300

    commit

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -3,14 +3,7 @@ package ru.yandex.practicum.filmorate.service.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exceptions.ValidationException;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Service
 public class UserService {
