1cd7e02ae38001f610ef78be7d618af6de70faea
Nikita <nikita.pro.pro@yandex.ru>
2023-03-31 02:21:23 +0300

    commit

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exceptions.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
 import java.util.*;
