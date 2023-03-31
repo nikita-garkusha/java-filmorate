d2369ca65137e72d8b01172dcea1557ba36bf9ef
Nikita <nikita.pro.pro@yandex.ru>
2023-03-31 19:13:40 +0300

    Внесение правок 2.0

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -6,6 +6,7 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 @Service
 public class UserService {
     private UserStorage userStorage;
+
     public UserService(UserStorage userStorage) {
         this.userStorage = userStorage;
     }
