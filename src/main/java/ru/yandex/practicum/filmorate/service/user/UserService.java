b4e99894317780377f9a80a7a44391c43363653c
Nikita <nikita.pro.pro@yandex.ru>
2023-04-02 17:25:12 +0300

    Тестовый коммит

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -8,8 +8,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.List;
-
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
