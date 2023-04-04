c9b607f308c674bb4d6d1d69de445bde55fc024b
Nikita <nikita.pro.pro@yandex.ru>
2023-04-04 15:10:16 +0300

    тестовый коммит

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmNotFoundException.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class FilmNotFoundException extends IllegalArgumentException {
+
     public FilmNotFoundException(String message) {
         super(message);
         log.error(message);
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/MpaNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/MpaNotFoundException.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MpaNotFoundException extends IllegalArgumentException {
+
     public MpaNotFoundException(String message) {
         super(message);
         log.error(message);
