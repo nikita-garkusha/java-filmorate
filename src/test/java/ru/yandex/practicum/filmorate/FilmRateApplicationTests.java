17cc68c6337ac05542747c006ab67cfc615400b2
Nikita <nikita.pro.pro@yandex.ru>
2023-04-04 15:22:53 +0300

    тестовый коммит

---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmRateApplicationTests.java
@@ -0,0 +1,62 @@
+package ru.yandex.practicum.filmorate;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.film.FilmService;
+import ru.yandex.practicum.filmorate.service.user.UserService;
+import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+@AutoConfigureCache
+@Transactional
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class FilmRateApplicationTests {
+    private final UserDbStorage userStorage;
+    private final FilmDbStorage filmStorage;
+    private final FilmService filmService;
+    private final UserService userService;
+    private User firstUser;
+    private User secondUser;
+    private User thirdUser;
+    private Film firstFilm;
+    private Film secondFilm;
+    private Film thirdFilm;
+
+    @BeforeEach
+    public void beforeEach() {
+        firstUser = User.builder()
+                .name("MisterFirst")
+                .login("First")
+                .email("1@ya.ru")
+                .birthday(LocalDate.of(1980, 12, 23))
+                .build();
+
+        secondUser = User.builder()
+                .name("MisterSecond")
+                .login("Second")
+                .email("2@ya.ru")
+                .birthday(LocalDate.of(1980, 12, 24))
+                .build();
+
+        thirdUser = User.builder()
+                .name("MisterThird")
+                .login("Third")
+                .email("3@ya.ru")
+                .birthday(LocalDate.of(1980, 12, 25))
+                .build();
+
+    }
+}
